208908060e1817986e75b5e16c724e89a6842e77
taha <mot1922092@sicsr.ac.in>
2022-02-24 15:47:11 +0300

    fetched data from the backend

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,39 +1,21 @@
 import React, {useState, useEffect} from 'react'
 import "./About.scss"
 import {motion} from 'framer-motion'
-
+import { urlFor, client } from '../../client'
 import {images} from "../../constants"
 
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I eat shit",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Web Design",
-    description: "I eat shit",
-    imgUrl: images.about02,
-    
-  },
-  {
-    title: "UI/UX",
-    description: "I eat shit",
-    imgUrl: images.about03,
-    
-  },
-  {
-    title: "Frontend Development",
-    description: "I eat shit",
-    imgUrl: images.about04,
-    
-  }
-]
-
 const About = () => {
+
+  const [abouts, setAbouts] = useState([])
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+    client.fetch(query)
+    .then( (data) => {setAbouts(data) })
+  }, [])
+  
   return (
       <>
-        <h2 className='head-text'> I know that 
+        <h2 className='head-text' style={{marginTop: "2rem"}}> I know that 
         <span> Good Design </span> <br />
         means
         <span> Good Business </span>
@@ -49,7 +31,7 @@ const About = () => {
               key={`${about.title} ${index}`}
               >
               
-              <img src={about.imgUrl} alt={about.title}/>
+              <img src={urlFor(about.imgUrl)} alt={about.title}/>
               <h2 className='bold-text' style={{ marginTop: 20}}> {about.title} </h2>
               <p className='p-text' style={{ marginTop: 10}}> {about.description} </p>
 
