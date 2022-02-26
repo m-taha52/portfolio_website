e6b81ce18c35781fcde4d143cacd1c021f7329f4
taha <mot1922092@sicsr.ac.in>
2022-02-27 02:04:51 +0300

    created motionwrap and completed skills

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,7 +1,7 @@
 
 import React, {useState, useEffect} from 'react'
 import {motion} from "framer-motion"
-import {AppWrap} from "../../wrapper"
+import {AppWrap, MotionWrap} from "../../wrapper"
 import {urlFor, client} from "../../client"
 import ReactTooltip from "react-tooltip"
 
@@ -96,4 +96,4 @@ const Skills = () => {
   )
 }
 
-export default AppWrap(Skills, "skills")
+export default AppWrap(MotionWrap(Skills, "app__skills"), "skills", "app__whitebg")
